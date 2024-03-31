4b96111a764d1b9527bfc7a6954e4f99342e51f0
luiza <luiza@DESKTOP-82K6BJH>
2024-03-31 16:33:01 -0300

    mudanças temporarias

--- src/main/java/controller/MatriculaServlet.java
+++ src/main/java/controller/MatriculaServlet.java
@@ -1,11 +1,18 @@
 package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import model.Disciplina;
+import persistence.DisciplinaDao;
+import persistence.GenericDao;
 
 /**
  * Servlet implementation class MatriculaServlet
@@ -25,8 +32,26 @@ public class MatriculaServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String erro = "";
+		List<Disciplina> disciplinas = new ArrayList<>();
+		
+		GenericDao gDao = new GenericDao();
+		DisciplinaDao dDao = new DisciplinaDao(gDao);
+		try {
+			disciplinas = dDao.listar();
+			for(Disciplina d : disciplinas) {
+				
+			}
+		} catch (ClassNotFoundException | SQLException e){
+			erro = e.getMessage();
+		} finally {
+			request.setAttribute("erro", erro);
+			request.setAttribute("disciplinas", disciplinas);
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("disciplina.jsp");
+		rd.forward(request, response);
+		
 	}
 
 	/**
---
+++ src/main/java/persistence/IDisciplina.java
@@ -0,0 +1,12 @@
+package persistence;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.MatriculaDisciplinas;
+
+public interface IDisciplina {
+	public List<Disciplina> 
+	public void inserirEmMatricula(int codigoMatricula, int codigoDisciplina, String situacao) throws SQLException, ClassNotFoundException;
+	public List<MatriculaDisciplinas> listarSituacao(int codigoMatricula, int codigoDisciplina) throws SQLException, ClassNotFoundException;
+}
